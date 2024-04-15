db943f6def107c2327f366f892fd079bcebacf82
WiktorSroda <wiktorsroda06@gmail.com>
2024-04-15 09:19:41 +0000

    wyslanie pracy

---
+++ python/test/main.py
@@ -0,0 +1,13 @@
+'''
+x = 10
+y = 5
+if x < y:
+    print("x jest mniejsze od y")
+else:
+    print("x nie jest mniejszy od y")
+'''
+a = int(input("podaj A"))
+b = int(input("podaj B"))
+print(a+b)
+
+
